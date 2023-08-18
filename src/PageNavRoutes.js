c457ef09dd56d0e8868be0c9d2cc6f6229949a03
alexlopez11 <106716844+alexlopez11@users.noreply.github.com>
2023-08-18 13:41:14 -0400

    Added Rainforest gallery page
    Added Rainforest gallery page

--- src/PageNavRoutes.js
+++ src/PageNavRoutes.js
@@ -8,13 +8,15 @@ import { Route, Routes } from "react-router-dom";
 import AboutUs from './About Us';
 import HomePage from "./HomePage";
 import Wildanimal from "./Wildanimalsgallery";
+import Wildplants from './RainforestGallery';
 
 
 function PageNavRoutes() {
     return(
        <Routes>
             <Route path='/' element={<HomePage />} />
-            <Route path='/wildanimals' element={<Wildanimal />} />            
+            <Route path='/wildanimals' element={<Wildanimal />} />
+            <Route path='/rainforest' element={<Wildplants />} />            
             <Route path='/aboutus' element={<AboutUs />} />
        </Routes>
         
---
+++ src/RainforestGallery.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+
+/* 
+    Created by Alex Lopez
+    Student ID: 8808489
+*/
+
+
+function Wildplants() {
+
+    const navigate = useNavigate();
+
+    const navcustomer = async (e) => {
+        e.preventDefault();
+        navigate(`/customerlogin`);
+    }
+
+    const navservicepro = async (e) => {
+        e.preventDefault();
+        navigate(`/serviceproviderlogin`);
+    }
+
+
+    return (
+
+
+        <div>
+
+            <div id="headdescription">
+                <h3 id="descripHead">Journey into the Heart of the Rainforest: Capturing the Mystique of Earth's Lush Green Canopy. </h3>
+            </div>
+
+            <div class="outdiv">
+
+                <div class="innDivplant">
+
+                    <img class="portAni" src="/imgs/pl.jpg" ></img>
+
+                </div>
+
+                <div class="landdivplants">
+                    <img class="landAni" src="/imgs/pl1.jpg" ></img>
+                    <img class="landAni" src="/imgs/pl2.jpg" ></img>
+                    <img class="landAni" src="/imgs/pl6.jpg" ></img>
+                    <img class="landAni" src="/imgs/pl4.jpg" ></img>
+                    <img class="landAni" src="/imgs/pl5.jpg" ></img>
+                    <img class="landAni" src="/imgs/pl3.jpg" ></img>
+                    <img class="landAni" src="/imgs/pl7.jpg" ></img>
+                    <img class="landAni" src="/imgs/pl8.jpg" ></img>
+                    <img class="landAni" src="/imgs/pl9.jpg" ></img>
+                </div>
+
+            </div>
+
+        </div>
+    )
+
+}
+
+export default Wildplants;
