dee849652e582ae57c9fae0ef7742db1e6bb4741
bibinJohn23 <bibinjohnjacob4047@conestogac.on.ca>
2023-08-18 01:20:49 -0400

    Wild Animals Gallery

--- src/PageNavRoutes.js
+++ src/PageNavRoutes.js
@@ -7,14 +7,15 @@ import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import AboutUs from './About Us';
 import HomePage from "./HomePage";
+import Wildanimal from "./Wildanimalsgallery";
 
 
 function PageNavRoutes() {
     return(
        <Routes>
             <Route path='/' element={<HomePage />} />
+            <Route path='/wildanimals' element={<Wildanimal />} />            
             <Route path='/aboutus' element={<AboutUs />} />
-
        </Routes>
         
     )
---
+++ src/Wildanimalsgallery.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+// PAGE DONE BY BIBIN JOHN JACOB
+
+function Wildanimal() {
+    return (
+
+        <div class="containerWildanimal">
+
+            <div id="headdescription">
+                <h3 id="descripHead">Capturing the Soul of the Wild: Explore the Majesty of Nature Through Our Lens. </h3>
+            </div>
+
+            <div class="outdivWildanimal">
+
+                <div class="innDivWildanimal">
+
+                    <img class="portAniWildanimal" src="/imgs/p1.jpg" ></img>
+                    <img class="portAniWildanimal" src="/imgs/p3.jpg" ></img>
+                    <img class="portAniWildanimal" src="/imgs/p2.jpg" ></img>
+
+                </div>
+
+                <div class="landdivWildanimal">
+                    <img class="landAniWildanimal" src="/imgs/l1.jpg" ></img>
+                    <img class="landAniWildanimal" src="/imgs/l2.jpg" ></img>
+                    <img class="landAniWildanimal" src="/imgs/l6.jpg" ></img>
+                    <img class="landAniWildanimal" src="/imgs/l4.jpg" ></img>
+                    <img class="landAniWildanimal" src="/imgs/l5.jpg" ></img>
+                    <img class="landAniWildanimal" src="/imgs/l3.jpg" ></img>
+                    <img class="landAniWildanimal" src="/imgs/l7.jpg" ></img>
+                    <img class="landAniWildanimal" src="/imgs/l8.jpg" ></img>
+                    <img class="landAniWildanimal" src="/imgs/l9.jpg" ></img>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Wildanimal;
