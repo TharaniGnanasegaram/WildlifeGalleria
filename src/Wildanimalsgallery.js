3d67ddd5e47e8237a95d64d542e7261183c0a8a6
bibinJohn23 <bibinjohnjacob4047@conestogac.on.ca>
2023-08-18 01:24:02 -0400

    Wildanimal update

--- src/Wildanimalsgallery.js
+++ src/Wildanimalsgallery.js
@@ -15,22 +15,22 @@ function Wildanimal() {
 
                 <div class="innDivWildanimal">
 
-                    <img class="portAniWildanimal" src="/imgs/p1.jpg" ></img>
-                    <img class="portAniWildanimal" src="/imgs/p3.jpg" ></img>
-                    <img class="portAniWildanimal" src="/imgs/p2.jpg" ></img>
+                    <img class="portAniWildanimal" alt="portAniWildanimal" src="/imgs/p1.jpg" ></img>
+                    <img class="portAniWildanimal" alt="portAniWildanimal" src="/imgs/p3.jpg" ></img>
+                    <img class="portAniWildanimal" alt="portAniWildanimal" src="/imgs/p2.jpg" ></img>
 
                 </div>
 
                 <div class="landdivWildanimal">
-                    <img class="landAniWildanimal" src="/imgs/l1.jpg" ></img>
-                    <img class="landAniWildanimal" src="/imgs/l2.jpg" ></img>
-                    <img class="landAniWildanimal" src="/imgs/l6.jpg" ></img>
-                    <img class="landAniWildanimal" src="/imgs/l4.jpg" ></img>
-                    <img class="landAniWildanimal" src="/imgs/l5.jpg" ></img>
-                    <img class="landAniWildanimal" src="/imgs/l3.jpg" ></img>
-                    <img class="landAniWildanimal" src="/imgs/l7.jpg" ></img>
-                    <img class="landAniWildanimal" src="/imgs/l8.jpg" ></img>
-                    <img class="landAniWildanimal" src="/imgs/l9.jpg" ></img>
+                    <img class="landAniWildanimal" alt="portAniWildanimal" src="/imgs/l1.jpg" ></img>
+                    <img class="landAniWildanimal" alt="portAniWildanimal" src="/imgs/l2.jpg" ></img>
+                    <img class="landAniWildanimal" alt="portAniWildanimal" src="/imgs/l6.jpg" ></img>
+                    <img class="landAniWildanimal" alt="portAniWildanimal" src="/imgs/l4.jpg" ></img>
+                    <img class="landAniWildanimal" alt="portAniWildanimal" src="/imgs/l5.jpg" ></img>
+                    <img class="landAniWildanimal" alt="portAniWildanimal" src="/imgs/l3.jpg" ></img>
+                    <img class="landAniWildanimal" alt="portAniWildanimal" src="/imgs/l7.jpg" ></img>
+                    <img class="landAniWildanimal" alt="portAniWildanimal" src="/imgs/l8.jpg" ></img>
+                    <img class="landAniWildanimal" alt="portAniWildanimal" src="/imgs/l9.jpg" ></img>
                 </div>
 
             </div>
